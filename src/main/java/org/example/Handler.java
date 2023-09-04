5de153159367adb300386a485a69078cf3d7dbd3
Ann Chapyrkina <chapyrkinaa@mail.ru>
2023-09-04 16:23:31 +0400

    Initial commit

---
+++ src/main/java/org/example/Handler.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
+public interface Handler {
+
+    void handle(Request request, BufferedOutputStream responseStream) throws IOException;
+}
